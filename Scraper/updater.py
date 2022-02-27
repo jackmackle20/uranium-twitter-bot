fab06550a6594876894a6a5f7f605495fe640559
JACK <jackmackle@icloud.com>
2022-02-27 14:23:19 -0500

    removing storage

--- Scraper/updater.py
+++ Scraper/updater.py
@@ -5,10 +5,10 @@ from datetime import date
 def update_database():
     today = date.today()
 
-    pull_tweets.write_to_db(f"'#uranium' since:{today} -filter:retweets",
-                            "storage/test.db",
-                            "test.db",
-                            "replace")
+    pull_tweets.write_to_db(f"'#uranium' since:2022-02-22 -filter:retweets",
+                            "storage/storage.db",
+                            "storage.db",
+                            "append")
 
 if __name__ == "__main__":
     update_database()
