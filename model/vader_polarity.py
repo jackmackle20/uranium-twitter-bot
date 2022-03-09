a7154faf9192cacad78d8767c23c776a9d371899
JACK <jackmackle@icloud.com>
2022-03-09 07:40:17 -0500

    polarity WIP

---
+++ model/vader_polarity.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np 
+import sqlite3
+import nltk
+import re
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+def query_db():
+    db_path = os.path.dirname(os.path.realpath("storage/storage.db"))
+    db_filename = os.path.join(db_path, "storage.db")
+    
+    conn = sqlite3.connect(db_filename)
+    
+    df = pd.read_sql_query("SELECT * FROM tweets", conn)
+    
+    conn.close()
+    
+    return df 
+
+def do_vader():
+    sid = SentimentIntensityAnalyzer()
+    
+    df = query_db()
+    
+    tw_text = df["full_text"].tolist()
+    
+    
