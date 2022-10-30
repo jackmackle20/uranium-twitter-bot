69ebc258f82c96916b5f531f87b98a78877a92da
unknown <jackmackle@icloud.com>
2022-10-30 09:33:38 -0400

    tinkering with sqlalchemy

--- tests/gcp_connection.py
+++ tests/gcp_connection.py
@@ -1,5 +1,5 @@
 from google.cloud.sql.connector import Connector, IPTypes
-import sqlalchemy
+import sqlalchemy as db
 import json
 
 with open("secrets.json", "r") as s:
@@ -17,7 +17,19 @@ def getconn():
         )
         return conn
 
-engine = sqlalchemy.create_engine(
+engine = db.create_engine(
     "postgresql+pg8000://",
-    creator=getconn,
-)
+    creator=getconn())
+
+metadata = db.MetaData()
+
+profile = db.Table(
+    'test_table',
+    metadata,
+    db.Column('col1', db.String, primary_key=True),
+    db.Column('Col2', db.Integer))
+
+metadata.create_all(engine)
+
+
+    
